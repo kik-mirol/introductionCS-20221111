ab4d321121e32d019af6eb7c55db2dd89af49b42
Feliks <ms.blackliss6292@gmail.com>
2022-12-02 00:44:17 +0400

    02z

---
+++ 02/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//2.С клавиатуры вводятся два вещественных числа. Проверять является ли одно из них квадратом второго
+System.Console.WriteLine("введите число a:");
+double a=Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("введите число b:");
+double b=Convert.ToDouble(Console.ReadLine());
+if (a*a==b) System.Console.WriteLine($"{b} является квадратом {a}");
+if (b*b==a) System.Console.WriteLine($"{a} является квадратом {b}");
+/*
+{
+    //true
+}
+else
+{
+    //false
+}
+*/
