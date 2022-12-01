0ca7c08ee1649307448afe74f93e4cabd7784e95
Feliks <ms.blackliss6292@gmail.com>
2022-12-02 00:41:14 +0400

    01z

--- 01/Program.cs
+++ 01/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//#region Мой код
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//#endregion
+//char c='a';
+//string s="asfd";
+//s=s+"e";
+//System.Console.WriteLine("fdgdg"+"dfdg"+2+"gdgsrg");//метод
+//Console.WriteLine("2+2");
+/*
+int a=0,b=0,c=0;//целый
+float f=20:4;//32 бит
+double d=3.14;//вещественный 64 бит
+string s="dfghfgh";
+char c='a';
+bool flag=true;
+int переменная1=0;
+int @_45f;
+*/
+//1.С клавиатуры вводится целое число.Вывести квадрат числа
+int a;// 32 бие от -2000000000 до 2000000000
+a=10;
+System.Console.WriteLine("Введите число:");
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+int b=a*a;
+System.Console.WriteLine(b);
+
