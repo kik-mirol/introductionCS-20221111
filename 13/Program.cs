72c0681c979c6389a27232aa02a9bb0a98fa6811
Feliks <ms.blackliss6292@gmail.com>
2022-12-02 02:25:36 +0400

    13z

---
+++ 13/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//13.Удалить вторую цифру целого числа введенного с клавиатуры.(сколько цифр в числе должно быть? и удалить нужно 2 с конца или просто 2 цифру в числе?)
+int a=Convert.ToInt32(Console.ReadLine());
+int b=a / 100 * 10+a % 10;
+System.Console.WriteLine(b);
