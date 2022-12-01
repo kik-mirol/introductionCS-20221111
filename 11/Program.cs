eed10acd0d1d63635de5fefc0456fc267a933382
Feliks <ms.blackliss6292@gmail.com>
2022-12-02 02:10:23 +0400

    11z

---
+++ 11/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//11.Дано число больше 9. Вывести на экран  вторую цифру числа с конца
+int a=256;
+int b=a / 10;
+System.Console.WriteLine(b % 10);
