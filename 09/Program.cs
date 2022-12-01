38dfb3a9eeae25565af51c7b5ebd9aaaaf6677be
Feliks <ms.blackliss6292@gmail.com>
2022-12-02 01:55:42 +0400

    09z

---
+++ 09/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//9.Вывести на экран четные числа от 1 до N
+System.Console.WriteLine("введите число N:");
+int N=Convert.ToInt32(Console.ReadLine());
+int i=2;
+while(i<N+1)
+{
+    System.Console.WriteLine($"{i} ");i=i+2;
+}
