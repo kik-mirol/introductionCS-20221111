09cc7651769ce291f863386367277a658161ebf4
Feliks <ms.blackliss6292@gmail.com>
2022-12-02 02:02:39 +0400

    10z

---
+++ 10/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//10.Вывести на экран последнюю цифру целого числа введенного с клавиатуры.
+System.Console.WriteLine("введите число a:");
+int a=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(a % 10);
