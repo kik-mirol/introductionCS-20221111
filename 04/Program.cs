fd69743e350625083dc2e3e31d646e1b062218f7
Feliks <ms.blackliss6292@gmail.com>
2022-12-02 01:07:53 +0400

    04z

---
+++ 04/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+System.Console.WriteLine("Введите число от 1 до 7 включительно:");
+int a=Convert.ToInt32(Console.ReadLine());
+if(a==1)System.Console.WriteLine("понедельник");
+if(a==2)System.Console.WriteLine("вторник");
+if(a==3)System.Console.WriteLine("среда");
+if(a==4)System.Console.WriteLine("четверг");
+if(a==5)System.Console.WriteLine("пятница");
+if(a==6)System.Console.WriteLine("суббота");
+if(a==7)System.Console.WriteLine("воскресенье");
