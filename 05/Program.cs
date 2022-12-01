f9ad46850dec01644a950c990cc6db6949c33fb4
Feliks <ms.blackliss6292@gmail.com>
2022-12-02 01:21:45 +0400

    05z

---
+++ 05/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//5.С клавиатуры вводятся три числа. Найти максимальное из трех чисел
+System.Console.WriteLine("введите число a:");
+double a=Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("введите число b:");
+double b=Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("введите число c:");
+double c=Convert.ToDouble(Console.ReadLine());
+double m;
+
+if (a>b)
+{
+    m=a;
+}
+else
+{
+    m=b;
+}
+if (c>m) 
+{
+    m=c;
+}
+System.Console.WriteLine($"максимальное число {m}");
+
