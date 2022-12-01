c45a268c795ac1a1c1414bda2c0ac0694a897850
Feliks <ms.blackliss6292@gmail.com>
2022-12-02 01:33:31 +0400

    07z

---
+++ 07/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//7.Выяснить является ли число чётным.
+System.Console.WriteLine("введите число:");
+int a=Convert.ToInt32(Console.ReadLine());
+if ((a % 2) == 0)
+{
+    Console.WriteLine("чётное");
+}
+else
+{
+    Console.WriteLine("нечётное");
+}
