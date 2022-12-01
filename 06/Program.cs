29ebdb2244b7e9ef6e56d45ab83354f1a09e5a64
Feliks <ms.blackliss6292@gmail.com>
2022-12-02 01:28:21 +0400

    06z

---
+++ 06/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//6.Написать программу вычисления значения функции y = sin(a). (Класс Math). 
+double a=Convert.ToDouble(Console.ReadLine());
+double y=Math.Sin(a);
+System.Console.WriteLine(y);
