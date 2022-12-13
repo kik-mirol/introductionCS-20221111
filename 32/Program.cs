dec7e6804b3fb4baaf7f1cab779ab05b83af86c1
Feliks <ms.blackliss6292@gmail.com>
2022-12-13 20:07:07 +0400

    04

--- 32/Program.cs
+++ 32/Program.cs
@@ -1,7 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 //32.Задать массив из 8 целых элементов и вывести их на экран
-Console.WriteLine("Введите размер массива: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int[] numbers = new int[n];
-Console.WriteLine($"массив: ",numbers);
+int[] array = new int[8];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(-100,100);
+}
+Console.WriteLine(string.Join ("  ", array));
